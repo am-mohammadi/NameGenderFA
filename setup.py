652f3ec320810f4ba67118791178164c78f8ab33
amirmahdi mohammadi <mohamadi.mahdi.6@gmail.com>
2023-06-17 18:42:04 +0430

    description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="NameGenderFA",                     # This is the name of the package
     version="0.0.2",                        # The initial release version
     author="Amirmahdi Mohammadi",                     # Full name of the author
-    description="Detecting gender of Persian names with auto cleaning the input",
+    description="Detecting the gender of Persian names with automatic cleaning of input",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),    # List of all python modules to be installed
